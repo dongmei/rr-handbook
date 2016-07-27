46b54c7a9517c27e1f7e7c5e293b247d7255edf8
dongmei <zoe.94heather@gmail.com>
2016-07-27 20:56:53 +0800

    Fixed side nav bar background image. Add jQuery to load crocodilian chapter.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
     $('.nav').children().each(function() {
         var link = $(this).children("a").prop('href');
-        //console.log(window.location.href);
+        console.log(window.location.pathname);
         if (link == window.location.href) {
             $(this).addClass('active');
         } else {
@@ -11,14 +11,22 @@ $(document).ready(function() {
         }
     });
 
+
+    $('#main-panel').scrollspy({ target: '#side-nav' });
+
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
 
+    $("#chapter-crocodilians").ready(function(){
+        console.log("test");
+        $("#chapter-crocodilians").load("crocodilians.html");
 
-    if ($(window).width() >= 768) {
-        $('.section-content').addClass('in');
-    }
+        if ($(window).width() >= 768) {
+            $('.section-content').addClass('in');
+        }
+    });
 
+    $()
 });
